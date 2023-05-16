b4840ac4ecae4fd6141840fd2c2bf806eba4954d
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2023-05-16 11:09:55 -0400

    Add files via upload
    Needs event triggers added for notifications. this is skeleton prototype code and only proof of concept.

--- RFID website/index.js
+++ RFID website/index.js
@@ -1,29 +1,56 @@
-var BLE_service;
-var BLE_characteristic;
-var BLE_device_name;
-var BLE_data;
-var connected_BLE_device;
 
 
 
 
-//this function is just a test, needs to write RFID value to "RFID_Badge_number" in index.html
-function write_html_val(){
-    document.getElementById("RFID_Badge_number").innerHTML = "GOODBYE!";
-}
 
-function find_bluetooth(){
-    navigator.bluetooth.requestDevice({
+
+
+//THIS IS WHAT IS WORKING
+/*
+How it works:
+this javascript code uses "promises", which are objects that are basically incomplete, waiting to finish exicuting. 
+That is why "await" is used in front of all the "navigator.xxxx" functions. This allows many of these to run 
+at the same time without blocking.
+
+Issues
+# right now this code read the characteristic in a while loop which runs infinately.This must be replaced 
+by an event. Specifically notifications must cause the javascript code to update the value shown in the website. 
+
+#speghetti code must be cleaned up. 
+*/
+async function test(){
+    //device find
+    let device = await navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
-        optionalServices: ['battery_service'] // Required to access service later.
-      })
-      .then(device => { /* … */ })//promise waits for bluetooth device asynchronously
-      .catch(error => { console.error(error);
-                        window.alert("No bluetooth on device.") 
-                    });
-        
-}
+        optionalServices: ['battery_service']
+      });
+    document.getElementById("bluetooth_device_name").innerHTML = device.name;
 
+    console.log('Connecting to GATT Server...');
+    const server = await device.gatt.connect();
+    
+    console.log('Getting Battery Service...');
+    const service = await server.getPrimaryService('battery_service');
+    
+    console.log('Getting Battery Level Characteristic...');
+    const characteristic = await service.getCharacteristic('battery_level');
+   
+    if(characteristic.properties.notify){
+        //infinate loop needs to be replaced with event triggered by "notifications" from BLE
+        while(true){
+            val = await characteristic.readValue();
+            document.getElementById("RFID_Badge_number").innerHTML = val.getUint8(0);
+        }
+        
+    }
+    else {
+        window.alert("your bluetooth device doesnt have notifications enabled!");
+    }
+    
+    console.log("done");
+
+    
+}
 
 
 
