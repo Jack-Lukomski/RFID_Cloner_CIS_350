ab58624911f705d5da13161a056d6237e7db92eb
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2023-05-16 22:00:16 -0400

    Add files via upload
    event handler working on BLE notifications. 
    added drop down bar and buttons that arent hooked up to JS yet.

--- RFID website/index.js
+++ RFID website/index.js
@@ -35,14 +35,23 @@ async function test(){
     console.log('Getting Battery Level Characteristic...');
     const characteristic = await service.getCharacteristic('battery_level');
    
+    //if characteristic has notifications enabled
     if(characteristic.properties.notify){
-        //infinate loop needs to be replaced with event triggered by "notifications" from BLE
-        while(true){
-            val = await characteristic.readValue();
-            document.getElementById("RFID_Badge_number").innerHTML = val.getUint8(0);
-        }
+        //add event listener to characteristic, then if notification change val
+        characteristic.addEventListener(
+            "characteristicvaluechanged", 
+            //aero function: event is an event listener. this could be replaced with "_" or blank. It only serves as placeholder
+            async (event_handler) => {
+                //print new val to web page
+                val = await characteristic.readValue();
+                document.getElementById("RFID_Badge_number").innerHTML = val.getUint8(0);
+            }
+        )
+        //get updated characteristic if notification received
+        await characteristic.startNotifications();
         
     }
+    //you should have notifications enabled for live data!
     else {
         window.alert("your bluetooth device doesnt have notifications enabled!");
     }
